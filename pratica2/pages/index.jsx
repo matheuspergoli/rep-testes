4262433220666fb71f5b881a7e780921e9af469e
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-02 15:57:29 -0300

    pratica2

--- pratica2/pages/index.jsx
+++ pratica2/pages/index.jsx
@@ -1,7 +1,18 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 function Home() {
+	const router = useRouter()
+
+	function handleClick() {
+		const isConfirmed = confirm('Gostaria de ir para Produtos?')
+
+		if (isConfirmed) {
+			router.push('/produtos')
+		}
+	}
+
 	return (
 		<>
 			<Head>
@@ -22,6 +33,11 @@ function Home() {
 						</Link>
 					</li>
 				</ul>
+				<button
+					className='underline my-3 p-2 rounded-md border text-red-600'
+					onClick={handleClick}>
+					Navegação programatica
+				</button>
 			</main>
 		</>
 	)
