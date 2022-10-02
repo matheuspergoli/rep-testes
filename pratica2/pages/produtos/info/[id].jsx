72bddea6c27ff859b2542f2877ee7b8a6cb5be86
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-02 08:19:38 -0300

    pratica2

--- pratica2/pages/produtos/info/[id].jsx
+++ pratica2/pages/produtos/info/[id].jsx
@@ -9,27 +9,23 @@ export async function getServerSideProps(context) {
 	}
 }
 
-function Produto({ id }) {
+function InfoProduto({ id }) {
 	return (
 		<>
 			<Head>
 				<title>{id}</title>
 			</Head>
 			<main>
-				<h1 className='mb-5 text-5xl'>Página {id}</h1>
-				<p className='mb-2 text-2xl'>Essa é a Página {id}</p>
-				<Link href={`/produtos/info/${id}`}>
-					<a className='block w-fit my-3 p-2 rounded-md border text-red-600'>
-						Ver Informações
-					</a>
-				</Link>
+				<h1 className='mb-5 text-5xl'>Página de Informações</h1>
+				<p className='mb-2 text-2xl'>Essa é a Página de Informações do {id}</p>
+				<p className='text-xl text-red-500'>Preço: 3500R$</p>
 				<ul>
 					<li>
 						<Link href='/'>
 							<a className='underline'>Ir para Página Home</a>
 						</Link>
 					</li>
-					<li>
+          <li>
 						<Link href='/produtos'>
 							<a className='underline'>Ir para Página Produtos</a>
 						</Link>
@@ -40,4 +36,4 @@ function Produto({ id }) {
 	)
 }
 
-export default Produto
+export default InfoProduto
