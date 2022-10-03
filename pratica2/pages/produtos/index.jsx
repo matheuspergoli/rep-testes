7c5e1fc984e7840c4dfc7bd2ddca9d73329adf0e
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-03 08:24:28 -0300

    pratica2

--- pratica2/pages/produtos/index.jsx
+++ pratica2/pages/produtos/index.jsx
@@ -1,7 +1,21 @@
+import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 
 function Produtos() {
+	const [data, setData] = React.useState()
+
+	React.useEffect(() => {
+		async function fetchData() {
+			const response = await fetch('/api/produtos')
+			const json = await response.json()
+			setData(json)
+		}
+		fetchData()
+	}, [])
+
+	console.log(data)
+
 	return (
 		<>
 			<Head>
@@ -12,12 +26,14 @@ function Produtos() {
 				<p className='mb-2 text-2xl'>Essa é a Página Produtos</p>
 				<section className='w-fit p-2 border rounded-md'>
 					<h2 className='text-2xl'>Ver Produtos</h2>
-					<Link href='/produtos/Notebook'>
-						<a className='block underline text-red-500'>Notebook</a>
-					</Link>
-					<Link href='/produtos/Computador'>
-						<a className='block underline text-red-500'>Computador</a>
-					</Link>
+					{data &&
+						data.map((produto) => (
+							<Link key={produto.id} href={`/produtos/${produto.produto}`}>
+								<a className='block underline text-red-500'>
+									{produto.produto}
+								</a>
+							</Link>
+						))}
 				</section>
 				<ul>
 					<li>
--- pratica2/pages/produtos/info/[id].jsx
+++ pratica2/pages/produtos/info/[id].jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 
@@ -10,22 +11,36 @@ export async function getServerSideProps(context) {
 }
 
 function InfoProduto({ id }) {
+	const [data, setData] = React.useState()
+
+	React.useEffect(() => {
+		async function fetchData() {
+			const response = await fetch('/api/produtos')
+			const json = await response.json()
+			const produto = json.filter((item) => item.produto === id)
+			setData(produto[0])
+		}
+		fetchData()
+	}, [id])
+
 	return (
 		<>
 			<Head>
-				<title>{id}</title>
+				<title>{data?.produto}</title>
 			</Head>
 			<main>
 				<h1 className='mb-5 text-5xl'>Página de Informações</h1>
-				<p className='mb-2 text-2xl'>Essa é a Página de Informações do {id}</p>
-				<p className='text-xl text-red-500'>Preço: 3500R$</p>
+				<p className='mb-2 text-2xl'>
+					Essa é a Página de Informações do {data?.produto}
+				</p>
+				<p className='text-xl text-red-500'>Preço: R${data?.preco}</p>
 				<ul>
 					<li>
 						<Link href='/'>
 							<a className='underline'>Ir para Página Home</a>
 						</Link>
 					</li>
-          <li>
+					<li>
 						<Link href='/produtos'>
 							<a className='underline'>Ir para Página Produtos</a>
 						</Link>
