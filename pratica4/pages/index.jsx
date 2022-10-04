3419439765c559efe72a02ab88bdae7d84cc1600
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-04 00:53:32 -0300

    pratica4

---
+++ pratica4/pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+function Home() {
+	return (
+		<>
+			<Head>
+				<title>Home</title>
+			</Head>
+			<main>
+				<h1>NextJS App</h1>
+			</main>
+		</>
+	)
+}
+
+export default Home
