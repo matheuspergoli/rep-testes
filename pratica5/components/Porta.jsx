b012f9d17e442663cfd4ef3ff6ad6fd0cd27b93d
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-05 10:12:32 -0300

    pratica5

--- pratica5/components/Porta.jsx
+++ pratica5/components/Porta.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import styles from '../styles/Porta.module.css'
 
 function Porta(props) {
-  return (
-    <div className={styles.porta}>
-      <div className={styles.numero}>3</div>
-    </div>
-  )
+	return (
+		<div className={styles.area}>
+			<div className={styles.estrutura}>
+				<div className={styles.porta}>
+					<div className={styles.numero}>3</div>
+				</div>
+			</div>
+      <div className={styles.chao}></div>
+		</div>
+	)
 }
 
 export default Porta
