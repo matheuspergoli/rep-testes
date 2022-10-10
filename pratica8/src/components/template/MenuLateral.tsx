b89f7490ac7a56451e6f087ad3a82bf1442fc86f
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-10 16:57:04 -0300

    pratica8

--- pratica8/src/components/template/MenuLateral.tsx
+++ pratica8/src/components/template/MenuLateral.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import { IconeAjustes, IconeCasa, IconeSino } from '../Icons'
+import { IconeAjustes, IconeCasa, IconeSair, IconeSino } from '../Icons'
 import Logo from './Logo'
 import MenuItem from './MenuItem'
 
 function MenuLateral() {
 	return (
-		<aside>
+		<aside className='flex flex-col'>
 			<div className='flex flex-col items-center justify-center h-20 w-20 bg-gradient-to-r from-indigo-500 to-purple-800'>
 				<Logo />
 			</div>
-			<ul>
+			<ul className='flex-grow'>
 				<MenuItem url='/' texto='Início' icone={IconeCasa} />
 				<MenuItem url='/ajustes' texto='Ajustes' icone={IconeAjustes} />
 				<MenuItem url='/notificacoes' texto='Notificações' icone={IconeSino} />
+			</ul>
+			<ul>
+				<MenuItem
+					onClick={() => console.log('Logout')}
+					texto='Sair'
+					icone={IconeSair}
+					className='hover:bg-red-400 hover:text-white text-red-600'
+				/>
 			</ul>
 		</aside>
 	)
