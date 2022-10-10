dd547855b6b8f54b518a15528cee46ca17d8d901
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-10 01:23:33 -0300

    pratica7

--- pratica7/pages/api/produtos.jsx
+++ pratica7/pages/api/produtos.jsx
@@ -1,10 +1,14 @@
+function numeroAleatorio(min = 1, max = 500) {
+	return Math.floor(Math.random() * (max - min)) + min
+}
+
 function Produtos(req, res) {
 	return res.status(200).json([
-		{ id: 1, nome: 'Caneta', preco: 5.6 },
-		{ id: 2, nome: 'Caderno', preco: 15.6 },
-		{ id: 3, nome: 'Borracha', preco: 7.3 },
-		{ id: 4, nome: 'Tesoura', preco: 21.55 },
-		{ id: 5, nome: 'Estojo', preco: 10.5 }
+		{ id: numeroAleatorio(), nome: 'Caneta', preco: 5.6 },
+		{ id: numeroAleatorio(), nome: 'Caderno', preco: 15.6 },
+		{ id: numeroAleatorio(), nome: 'Borracha', preco: 7.3 },
+		{ id: numeroAleatorio(), nome: 'Tesoura', preco: 21.55 },
+		{ id: numeroAleatorio(), nome: 'Estojo', preco: 10.5 }
 	])
 }
 
