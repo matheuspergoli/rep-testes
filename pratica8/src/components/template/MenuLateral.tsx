7aa81f4976974dc6caa33ef9e569aa2d8a04522f
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-10 16:37:46 -0300

    pratica8

--- pratica8/src/components/template/MenuLateral.tsx
+++ pratica8/src/components/template/MenuLateral.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { IconeAjustes, IconeCasa, IconeSino } from '../Icons'
+import Logo from './Logo'
 import MenuItem from './MenuItem'
 
 function MenuLateral() {
 	return (
 		<aside>
+			<div className='flex flex-col items-center justify-center h-20 w-20 bg-gradient-to-r from-indigo-500 to-purple-800'>
+				<Logo />
+			</div>
 			<ul>
 				<MenuItem url='/' texto='Início' icone={IconeCasa} />
 				<MenuItem url='/ajustes' texto='Ajustes' icone={IconeAjustes} />
