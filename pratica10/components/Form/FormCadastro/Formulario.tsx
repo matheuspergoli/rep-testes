fea3265d7784cdcaca4ef72feb20eb2df50163f8
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-13 17:41:50 -0300

    pratica10

--- pratica10/components/Form/FormCadastro/Formulario.tsx
+++ pratica10/components/Form/FormCadastro/Formulario.tsx
@@ -1,11 +1,36 @@
 import React from 'react'
 import Input from './Input'
+import { useFormik } from 'formik'
+import { CadastroSchema } from '../../../model/CadastroSchema'
 
 function Formulario() {
+	const formik = useFormik({
+		initialValues: {
+			senha: '',
+			email: ''
+		},
+		validationSchema: CadastroSchema,
+		onSubmit: () => null
+	})
+
 	return (
-		<form className='flex flex-col gap-2'>
-			<Input type='email' placeholder='Digite seu email' />
-			<Input type='password' placeholder='Digite sua senha' />
+		<form className='flex flex-col gap-2' onSubmit={formik.handleSubmit}>
+			<Input
+				type='email'
+				placeholder='Digite seu email'
+				value={formik.values.email}
+				onChange={formik.handleChange}
+			/>
+			{formik.errors.email ? <p className='text-red-500'>{formik.errors.email}</p> : null}
+
+			<Input
+				type='password'
+				placeholder='Digite sua senha'
+				value={formik.values.senha}
+				onChange={formik.handleChange}
+			/>
+			{formik.errors.senha ? <p className='text-red-500'>{formik.errors.senha}</p> : null}
+
 			<button className='p-2 border rounded-md bg-black text-white' type='submit'>
 				Cadastrar
 			</button>
--- pratica10/components/Form/FormCadastro/Input.tsx
+++ pratica10/components/Form/FormCadastro/Input.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 interface InputProps {
 	type: string
 	placeholder: string
-	value?: string
-	onChange?: any
+	value: string
+	onChange: any
 }
 
 function Input(props: InputProps) {
 	return (
 		<input
-      className='p-2 rounded-md border focus:outline-none placeholder:text-black bg-gray-300 focus:bg-white focus:border-blue-400'
+			className='p-2 rounded-md border focus:outline-none placeholder:text-black bg-gray-300 focus:bg-white focus:border-blue-400'
 			type={props.type}
 			name={props.type}
 			placeholder={props.placeholder}
