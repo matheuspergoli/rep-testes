3952fc901b3e517e9a57f4ec18cbf872ff9d8fa2
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-21 20:21:59 -0300

    pratica15

--- pratica15/src/test/Button.test.jsx
+++ pratica15/src/test/Button.test.jsx
@@ -1,5 +1,5 @@
 import { describe, expect, it, vi, vitest } from 'vitest'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Button from '../components/Button'
 
@@ -7,16 +7,20 @@ describe('<Button />', () => {
 	it('Should render the text', () => {
 		render(<Button />)
 
+		expect.assertions(1)
+
 		const text = screen.getByText('Clique aqui')
 		expect(text).toBeInTheDocument()
 	})
 
-	it('Should call function on button click', () => {
+	it('Should call function on button click', async () => {
 		const fn = vitest.fn()
 		render(<Button onClick={fn} />)
 
+		expect.assertions(1)
+
 		const button = screen.getByText('Clique aqui')
-		fireEvent.click(button)
+		await userEvent.click(button)
 		expect(fn).toBeCalledTimes(1)
 	})
 })
