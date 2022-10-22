e0fb111018009596cb4409d422751be2cb330fa5
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-22 13:55:31 -0300

    pratica16

--- pratica16/src/App.tsx
+++ pratica16/src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Header from './components/Header'
 
 function App() {
 	return (
 		<main>
-			<Header title='React App' />
+			<h1>React App</h1>
 		</main>
 	)
 }
