0bcb28bea0390b3d3dbaa1767307d0b6a6b90084
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-21 22:35:58 -0300

    pratica15

--- pratica15/src/components/Button.jsx
+++ pratica15/src/components/Button.jsx
@@ -5,7 +5,7 @@ function Button(props) {
 		<button
 			onClick={props.onClick}
 			className='px-4 py-1 rounded-md transition font-semibold bg-gray-400 hover:bg-gray-700 hover:text-white active:scale-95'>
-			Clique aqui
+			Clique aqui!
 		</button>
 	)
 }
--- pratica15/src/test/Button.test.jsx
+++ pratica15/src/test/Button.test.jsx
@@ -1,26 +1,37 @@
-import { describe, expect, it, vi, vitest } from 'vitest'
+import { describe, expect, it, vitest } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Button from '../components/Button'
 
 describe('<Button />', () => {
-	it('Should render the text', () => {
+	it('Should render', () => {
 		render(<Button />)
-
+		const button = screen.getByRole('button', { name: 'Clique aqui' })
 		expect.assertions(1)
-
-		const text = screen.getByText('Clique aqui')
-		expect(text).toBeInTheDocument()
+		expect(button).toBeInTheDocument()
 	})
 
-	it('Should call function on button click', async () => {
+	it('Should call function on click', async () => {
 		const fn = vitest.fn()
 		render(<Button onClick={fn} />)
-
+		const button = screen.getByRole('button', { name: 'Clique aqui' })
 		expect.assertions(1)
-
-		const button = screen.getByText('Clique aqui')
 		await userEvent.click(button)
-		expect(fn).toBeCalledTimes(1)
+		await userEvent.click(button)
+		expect(fn).toBeCalledTimes(2)
+	})
+
+	it('Should have the class active:scale-95', () => {
+		render(<Button />)
+		const button = screen.getByRole('button', { name: 'Clique aqui' })
+		expect.assertions(1)
+		expect(button).toHaveClass('active:scale-95')
+	})
+
+	it('Should match the snapshot', () => {
+		render(<Button />)
+		const button = screen.getByRole('button', { name: 'Clique aqui' })
+		expect.assertions(1)
+		expect(button).toMatchSnapshot()
 	})
 })
