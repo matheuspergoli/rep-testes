d68722424ed361ead726d4927ffdfe8c68ab0065
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-22 18:01:16 -0300

    pratica16

--- pratica16/src/App.tsx
+++ pratica16/src/App.tsx
@@ -1,9 +1,41 @@
 import React from 'react'
 
 function App() {
+	const [counter, setCounter] = React.useState(0)
+	const [inputValue, setInputValue] = React.useState('')
+
+	const increment = () => setCounter((prevCount) => prevCount + 1)
+	const decrement = () => setCounter((prevCount) => prevCount - 1)
+
 	return (
-		<main>
-			<h1>React App</h1>
+		<main className='p-5 h-screen w-screen bg-gray-900 text-white'>
+			<h1 className='mb-5 text-3xl font-bold'>React App</h1>
+			<p className='text-2xl'>Contador: {counter}</p>
+			<div className='flex gap-2 mt-5'>
+				<button
+					onClick={increment}
+					className='px-5 py-1 transition rounded-full bg-gray-600 hover:bg-gray-700'>
+					Acrescentar
+				</button>
+				<button
+					onClick={decrement}
+					disabled={counter === 0}
+					className='px-5 py-1 transition rounded-full bg-gray-600 hover:bg-gray-700 disabled:opacity-30'>
+					Decrementar
+				</button>
+			</div>
+			<div className='mt-5'>
+				<p>Valor do Input: {inputValue}</p>
+				<input
+					type='text'
+					name='text'
+					id='text'
+					placeholder='Digite algo...'
+					value={inputValue}
+					onChange={(e) => setInputValue(e.target.value)}
+					className='px-2 py-1 rounded-md outline-none bg-gray-700'
+				/>
+			</div>
 		</main>
 	)
 }
---
+++ pratica16/src/test/App.test.tsx
@@ -0,0 +1,23 @@
+import { describe, expect, it } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import App from '../App'
+
+describe('<App />', () => {
+	it('Should increment the counter by 1', async () => {
+		render(<App />)
+		const counter = screen.getByText('Contador: 0')
+		const btnIncrement = screen.getByRole('button', { name: 'Acrescentar' })
+		await userEvent.click(btnIncrement)
+		expect(counter.textContent).toBe('Contador: 1')
+	})
+
+	it('Should render the value of the input', async () => {
+		render(<App />)
+		const input = screen.getByPlaceholderText('Digite algo...')
+		const inputTextValue = screen.getByText('Valor do Input:')
+		await userEvent.type(input, 'Matheus Pergoli')
+		expect(inputTextValue.textContent).toBe('Valor do Input: Matheus Pergoli')
+		expect(input).toHaveValue('Matheus Pergoli')
+	})
+})
