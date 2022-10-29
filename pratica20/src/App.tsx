63e88332a091764bd8e9011512491c3ac8ad9017
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-28 22:42:20 -0300

    pratica20

--- pratica20/src/App.tsx
+++ pratica20/src/App.tsx
@@ -1,50 +1,24 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, A11y } from 'swiper'
+import { Navigation } from 'swiper'
 import 'swiper/css'
-import 'swiper/css/a11y'
 import 'swiper/css/navigation'
 
 function App() {
 	return (
 		<main>
 			<Swiper
-				modules={[Navigation, A11y]}
+				modules={[Navigation]}
 				spaceBetween={50}
 				slidesPerView={3}
 				navigation
 				pagination={{ clickable: true }}
 				className='max-w-5xl h-96'>
-				<SwiperSlide className='p-1 text-center rounded-md text-xl font-bold text-white bg-gray-900'>
-					1
-				</SwiperSlide>
-				<SwiperSlide className='p-1 text-center rounded-md text-xl font-bold text-white bg-gray-900'>
-					2
-				</SwiperSlide>
-				<SwiperSlide className='p-1 text-center rounded-md text-xl font-bold text-white bg-gray-900'>
-					3
-				</SwiperSlide>
-				<SwiperSlide className='p-1 text-center rounded-md text-xl font-bold text-white bg-gray-900'>
-					4
-				</SwiperSlide>
-				<SwiperSlide className='p-1 text-center rounded-md text-xl font-bold text-white bg-gray-900'>
-					5
-				</SwiperSlide>
-				<SwiperSlide className='p-1 text-center rounded-md text-xl font-bold text-white bg-gray-900'>
-					6
-				</SwiperSlide>
-				<SwiperSlide className='p-1 text-center rounded-md text-xl font-bold text-white bg-gray-900'>
-					7
-				</SwiperSlide>
-				<SwiperSlide className='p-1 text-center rounded-md text-xl font-bold text-white bg-gray-900'>
-					8
-				</SwiperSlide>
-				<SwiperSlide className='p-1 text-center rounded-md text-xl font-bold text-white bg-gray-900'>
-					9
-				</SwiperSlide>
-				<SwiperSlide className='p-1 text-center rounded-md text-xl font-bold text-white bg-gray-900'>
-					10
-				</SwiperSlide>
+				{[1, 2, 3, 4, 5, 6].map((item) => (
+					<SwiperSlide className='p-1 text-center rounded-md text-xl font-bold text-white bg-gray-900'>
+						{item}
+					</SwiperSlide>
+				))}
 			</Swiper>
 		</main>
 	)
