83a7c7a65bcb257a6457c76b5bda4d31785e46b8
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-29 14:57:40 -0300

    pratica20

--- pratica20/src/App.tsx
+++ pratica20/src/App.tsx
@@ -9,14 +9,26 @@ function App() {
 		<main>
 			<Swiper
 				modules={[Navigation]}
-				spaceBetween={50}
-				slidesPerView={3}
+				spaceBetween={25}
 				navigation
+				slidesPerView={1}
+				breakpoints={{
+					640: { slidesPerView: 2 },
+					768: { slidesPerView: 3 }
+				}}
 				pagination={{ clickable: true }}
 				className='max-w-5xl h-96'>
-				{[1, 2, 3, 4, 5, 6].map((item) => (
-					<SwiperSlide className='p-1 text-center rounded-md text-xl font-bold text-white bg-gray-900'>
-						{item}
+				{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, index) => (
+					<SwiperSlide key={index} className='rounded-md'>
+						<section>
+							<h1 className='text-center font-semibold text-xl'>
+								Imagem {item}
+							</h1>
+							<img
+								src={`https://source.unsplash.com/random`}
+								className='w-full h-96 object-cover object-center'
+							/>
+						</section>
 					</SwiperSlide>
 				))}
 			</Swiper>
