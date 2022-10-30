2c33bf711155422f157598339fcc8244bddeb416
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-10-30 19:41:55 -0300

    pratica21

--- pratica21/src/components/CardMeal/index.tsx
+++ pratica21/src/components/CardMeal/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { useQuery } from 'react-query'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, FreeMode } from 'swiper'
+import 'swiper/css'
+import 'swiper/css/free-mode'
+import 'swiper/css/navigation'
 
 interface MealsProps {
 	meals: {
@@ -20,15 +25,29 @@ function CardMeal(props: { strCategory: string }) {
 
 	return (
 		<section className='m-5'>
-			<div className='flex flex-col gap-5'>
-				<h1 className='text-2xl font-bold mb-2'>{props.strCategory}</h1>
+			<h1 className='text-2xl font-bold mb-2'>{props.strCategory}</h1>
+			<Swiper
+				modules={[Navigation, FreeMode]}
+				slidesPerView={1}
+				spaceBetween={20}
+				navigation
+				freeMode
+				grabCursor
+				breakpoints={{
+					640: { slidesPerView: 2 },
+					768: { slidesPerView: 3 },
+					1024: { slidesPerView: 4 },
+					1280: { slidesPerView: 5 },
+					1536: { slidesPerView: 6 }
+				}}>
 				{data?.meals.map((meal) => (
-					<figure key={meal.idMeal} className='w-60 h-60'>
-						<figcaption>{meal.strMeal}</figcaption>
-						<img src={`${meal.strMealThumb}`} alt={`${meal.strMeal}`} className='w-full h-full' />
-					</figure>
+					<SwiperSlide key={meal.idMeal} className='text-center'>
+						<figure className='mx-auto w-60 h-60'>
+							<img src={`${meal.strMealThumb}`} alt={`${meal.strMeal}`} className='w-full h-full' />
+						</figure>
+					</SwiperSlide>
 				))}
-			</div>
+			</Swiper>
 		</section>
 	)
 }
