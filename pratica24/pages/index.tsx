4c73f9e4a60ea6d8f5ae553706509cb94ce64f92
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-05 17:32:20 -0300

    pratica24

--- pratica24/pages/index.tsx
+++ pratica24/pages/index.tsx
@@ -1,4 +1,28 @@
+import { dehydrate, QueryClient, useQuery } from 'react-query'
+
+async function getCountries() {
+	const response = await fetch('https://restcountries.com/v3.1/all')
+	const json = await response.json()
+	return json
+}
+
+export async function getServerSideProps() {
+	const queryClient = new QueryClient()
+
+	await queryClient.prefetchQuery(['countries'], getCountries)
+
+	return {
+		props: {
+			dehydratedState: dehydrate(queryClient)
+		}
+	}
+}
+
 function Home() {
+	const { data } = useQuery({ queryKey: ['countries'], queryFn: getCountries })
+
+	if (data) console.log(data)
+
 	return (
 		<main>
 			<h1>NextJS App</h1>
