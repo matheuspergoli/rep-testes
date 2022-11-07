1e7d88377fa8a91fb884fa1d4fb8d65a1ad25bff
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-07 18:07:24 -0300

    pratica29

--- pratica29/pages/index.tsx
+++ pratica29/pages/index.tsx
@@ -1,11 +1,8 @@
 import Head from 'next/head'
+import Slide from '../components/Slide'
 import { GetServerSideProps } from 'next'
 import Banner from '../components/Banner'
 import Profile from '../components/Profile'
-import VideoCard from '../components/VideoCard'
-import { Swiper, SwiperSlide } from 'swiper/react'
-
-import 'swiper/css'
 
 interface DataProps {
 	data: {
@@ -45,65 +42,9 @@ function Home(props: DataProps) {
 			<Banner />
 			<Profile />
 			<main className='px-2'>
-				<section>
-					<h2 className='text-xl font-bold mb-2'>Podcasts</h2>
-					<Swiper
-						breakpoints={{
-							320: { slidesPerView: 1 },
-							425: { slidesPerView: 2 },
-							768: { slidesPerView: 3 },
-							1024: { slidesPerView: 5 },
-							1230: { slidesPerView: 6 },
-							1440: { slidesPerView: 7 },
-							1536: { slidesPerView: 8 }
-						}}>
-						{props.data.podcasts.map((podcasts) => (
-							<SwiperSlide key={podcasts.title}>
-								<VideoCard {...podcasts} />
-							</SwiperSlide>
-						))}
-					</Swiper>
-				</section>
-
-				<section>
-					<h2 className='text-xl font-bold mb-2'>Músicas</h2>
-					<Swiper
-						breakpoints={{
-							320: { slidesPerView: 1 },
-							425: { slidesPerView: 2 },
-							768: { slidesPerView: 3 },
-							1024: { slidesPerView: 5 },
-							1230: { slidesPerView: 6 },
-							1440: { slidesPerView: 7 },
-							1536: { slidesPerView: 8 }
-						}}>
-						{props.data.musicas.map((musica) => (
-							<SwiperSlide key={musica.title}>
-								<VideoCard {...musica} />
-							</SwiperSlide>
-						))}
-					</Swiper>
-				</section>
-
-				<section>
-					<h2 className='text-xl font-bold mb-2'>Front-end</h2>
-					<Swiper
-						breakpoints={{
-							320: { slidesPerView: 1 },
-							425: { slidesPerView: 2 },
-							768: { slidesPerView: 3 },
-							1024: { slidesPerView: 5 },
-							1230: { slidesPerView: 6 },
-							1440: { slidesPerView: 7 },
-							1536: { slidesPerView: 8 }
-						}}>
-						{props.data.frontend.map((video) => (
-							<SwiperSlide key={video.title}>
-								<VideoCard {...video} />
-							</SwiperSlide>
-						))}
-					</Swiper>
-				</section>
+				<Slide title='Podcasts' data={props.data.podcasts} />
+				<Slide title='Músicas' data={props.data.musicas} />
+				<Slide title='Front-end' data={props.data.frontend} />
 			</main>
 		</>
 	)
