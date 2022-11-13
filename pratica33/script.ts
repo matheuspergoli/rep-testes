4f8d88e4cecea3cedd8bbf703e5948850b03aff4
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-13 17:37:05 -0300

    pratica33

---
+++ pratica33/script.ts
@@ -0,0 +1,32 @@
+async function fetchCursos() {
+	const response = await fetch('https://api.origamid.dev/json/cursos.json')
+	const data = await response.json()
+	mostrarCursos(data)
+}
+fetchCursos()
+
+interface Curso {
+	nome: string
+	horas: number
+	aulas: number
+	nivel: string
+	tags: string[]
+	idAulas: number[]
+	gratuito: boolean
+}
+
+function mostrarCursos(cursos: Curso[]) {
+	const cursosMapeados = cursos.map((curso) => {
+		return `<section>
+              <h1>${curso.nome}</h1>
+              <p>Horas: ${curso.horas}</p>
+              <p>Aulas: ${curso.aulas}</p>
+              <p>Nível: ${curso.nivel}</p>
+              <p>Gratuito: ${curso.gratuito ? 'sim' : 'não'}</p>
+            </section>`
+	})
+
+	cursosMapeados.forEach((curso) => {
+		document.body.innerHTML += curso
+	})
+}
