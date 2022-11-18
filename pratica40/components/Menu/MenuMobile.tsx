f81ff0c0f0498ab83dcefd5a38885a22a5e9203d
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-18 01:47:46 -0300

    pratica40

--- pratica40/components/Menu/MenuMobile.tsx
+++ pratica40/components/Menu/MenuMobile.tsx
@@ -1,32 +1,39 @@
 import React from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { MdClose as CloseIcon } from 'react-icons/md'
 import { GiHamburgerMenu as MenuIcon } from 'react-icons/gi'
 
 function MenuMobile() {
 	const router = useRouter()
+	const [isMenuMobileActive, setIsMenuMobileActive] = React.useState(false)
 	const headerRef = React.useRef<any>()
 
 	function showMenu() {
-		if (headerRef?.current?.classList.contains('hidden')) {
-			headerRef?.current?.classList.remove('hidden')
-			headerRef?.current?.classList.add('block')
-		} else {
-			headerRef?.current?.classList.remove('block')
-			headerRef?.current?.classList.add('hidden')
-		}
+		setIsMenuMobileActive((prev) => !prev)
 	}
 
 	function closeMenu() {
 		headerRef?.current.classList.add('hidden')
+		setIsMenuMobileActive(false)
 	}
 
 	return (
 		<>
-			<button className='block text-2xl w-fit ml-auto m-3 p-2 rounded-md md:hidden bg-main-black' onClick={showMenu}>
-				<MenuIcon className='text-white' />
+			<button
+				className={`${
+					isMenuMobileActive ? 'border-main-blue' : ''
+				} block border-4 text-2xl w-fit ml-auto m-3 p-2 rounded-md md:hidden bg-main-black`}
+				onClick={showMenu}>
+				{isMenuMobileActive ? (
+					<CloseIcon className={`${isMenuMobileActive ? 'text-main-blue' : 'text-white'}`} />
+				) : (
+					<MenuIcon className='text-white' />
+				)}
 			</button>
-			<header className='hidden absolute right-3 p-3 transition rounded-md md:hidden bg-main-black' ref={headerRef}>
+			<header
+				className={`${isMenuMobileActive ? 'block' : 'hidden'} absolute right-3 p-3 transition rounded-md md:hidden bg-main-black`}
+				ref={headerRef}>
 				<nav className='flex flex-col items-center gap-4 text-2xl text-white'>
 					<Link href='/' className={router.pathname === '/' ? 'text-main-blue' : ''} onClick={closeMenu}>
 						Home
