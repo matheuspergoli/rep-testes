600d350c925d5cdb5293d786886c6a2eb32bb70d
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-11-22 19:41:47 -0300

    pratica45

---
+++ pratica45/components/HeroPost/index.tsx
@@ -0,0 +1,19 @@
+function HeroPost(props: Post) {
+	return (
+		<article className='mb-40'>
+			<img src={props.coverImage.url} alt='Post cover' className='mb-10' />
+			<div className='grid grid-cols-1 px-3 gap-5 sm:p-0 lg:grid-cols-2 lg:gap-10'>
+				<h1 className='text-4xl'>{props.title}</h1>
+				<section>
+					<p className='text-2xl mb-5'>{props.excerpt}</p>
+					<div className='flex items-center gap-5'>
+						<img src={props.author.picture.url} alt='Author image' className='w-12 h-12 rounded-full' />
+						<p className='text-lg font-bold'>{props.author.name}</p>
+					</div>
+				</section>
+			</div>
+		</article>
+	)
+}
+
+export default HeroPost
--- pratica45/pages/index.tsx
+++ pratica45/pages/index.tsx
@@ -1,7 +1,10 @@
 import Head from 'next/head'
-import parse from 'html-react-parser'
+import Post from '../components/Post'
 import { GetServerSideProps } from 'next'
+import MainTitle from '../layout/MainTitle'
+import HeroPost from '../components/HeroPost'
 import getAllPosts from '../service/getAllPosts'
+import MainContainer from '../layout/MainContainer'
 import { dehydrate, QueryClient, useQuery } from 'react-query'
 
 export const getServerSideProps: GetServerSideProps = async () => {
@@ -27,35 +30,16 @@ function Home() {
 			<Head>
 				<title>Home</title>
 			</Head>
-			<main className='max-w-8xl mx-auto'>
-				<h2 className='text-7xl font-bold mb-10'>Blog.</h2>
-				<article className='mb-28'>
-					<img src={heroPost?.coverImage.url} alt='Post cover' className='mb-10' />
-					<div className='grid grid-cols-2 gap-10'>
-						<h1 className='text-4xl'>{heroPost?.title}</h1>
-						<section>
-							<p className='text-2xl mb-5'>{heroPost?.excerpt}</p>
-							<div className='flex items-center gap-5'>
-								<img src={heroPost?.author.picture.url} alt='Author image' className='w-12 h-12 rounded-full' />
-								<p className='text-lg font-bold'>{heroPost?.author.name}</p>
-							</div>
-						</section>
-					</div>
-				</article>
-				<div className='grid grid-cols-2 gap-20'>
+			<MainContainer>
+				<MainTitle>Blog.</MainTitle>
+				{heroPost ? <HeroPost {...heroPost} /> : null}
+				<h2 className='text-center text-5xl font-bold mb-20 sm:text-left sm:text-7xl'>Mais artigos</h2>
+				<div className='grid grid-cols-1 gap-20 lg:grid-cols-2'>
 					{morePosts?.map((post) => (
-						<section key={post.id} className='max-w-2xl w-full mx-auto'>
-							<img src={post.coverImage.url} alt='Post image' className='mb-3 w-full object-cover h-80' />
-							<h2 className='text-2xl mb-3'>{post.title}</h2>
-							<h3 className='text-lg mb-3'>{post.excerpt}</h3>
-							<div className='flex items-center gap-5'>
-								<img src={post.author.picture.url} alt='Author image' className='w-12 h-12 rounded-full' />
-								<p className='text-lg font-bold'>{post.author.name}</p>
-							</div>
-						</section>
+						<Post key={post.id} {...post} />
 					))}
 				</div>
-			</main>
+			</MainContainer>
 		</>
 	)
 }
