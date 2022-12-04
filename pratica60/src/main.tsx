3d10431994ebb1e94c1d318335dcc46e6d702c37
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-12-04 06:15:52 -0300

    pratica60

--- pratica60/src/main.tsx
+++ pratica60/src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyles } from './styles/GlobalStyles'
+import { AuthContextProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<GlobalStyles>
-				<App />
+				<AuthContextProvider>
+					<App />
+				</AuthContextProvider>
 			</GlobalStyles>
 		</BrowserRouter>
 	</React.StrictMode>
