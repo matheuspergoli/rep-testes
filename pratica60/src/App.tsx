229d9d44eb896753bb6ba9dbd1979c627036b904
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-12-04 09:47:18 -0300

    pratica60

--- pratica60/src/App.tsx
+++ pratica60/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Home } from './pages/Home'
 import { NewRoom } from './pages/NewRoom'
-import { Routes, Route } from 'react-router-dom'
+import { AuthContext } from './context/AuthContext'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 export const App = () => {
+	const { user } = React.useContext(AuthContext)
+
 	return (
 		<Routes>
 			<Route path='/' element={<Home />} />
-			<Route path='/rooms/new' element={<NewRoom />} />
+			<Route path='/rooms/new' element={user ? <NewRoom /> : <Navigate to='/' />} />
 		</Routes>
 	)
 }
--- pratica60/src/context/AuthContext.tsx
+++ pratica60/src/context/AuthContext.tsx
@@ -30,6 +30,24 @@ export const AuthContextProvider = (props: { children: React.ReactNode }) => {
 		setUser({ id: uid, name: displayName, avatar: photoURL })
 	}
 
+	React.useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged((user) => {
+			if (user) {
+				const { displayName, photoURL, uid } = user
+
+				if (!displayName || !photoURL) {
+					throw new Error('Missing information from Google Account.')
+				}
+
+				setUser({ id: uid, name: displayName, avatar: photoURL })
+			}
+		})
+
+		return () => {
+			unsubscribe()
+		}
+	}, [])
+
 	return (
 		<AuthContext.Provider value={{ user, signInWithGoogle }}>
 			{props.children}
