0dd427a9655ab443829e7b4bb0bb7471c49a5577
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-12-21 23:53:19 -0300

    pratica84

--- pratica84/components/MainContainer/index.tsx
+++ pratica84/components/MainContainer/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export const MainContainer = (props: { children: React.ReactNode }) => {
-	return <main className='mx-auto w-full max-w-7xl p-3'>{props.children}</main>
+	return <main className='container mx-auto p-3 sm:p-0'>{props.children}</main>
 }
