b6848fcce72eeb96657cc4216f009d6dbbcd74a0
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2023-01-01 13:13:38 -0300

    pratica95

--- pratica95/index.js
+++ pratica95/index.js
@@ -1,10 +1,40 @@
-import http from 'http'
+import express from 'express'
 
-const server = http.createServer((req, res) => {
-	res.writeHead(200, { 'Content-Type': 'text/html' })
-	res.end('<h1>Hello World!</h1>')
+const app = express()
+
+app.use(express.json())
+
+app.use('/login', (req, res, next) => {
+	const authorization = req.body.authorization
+	if (authorization) {
+		res.redirect('/login')
+		next()
+	} else {
+		res.redirect('/logado')
+		next()
+	}
 })
 
-server.listen(3000, () => {
-	console.log('Server is running on port 3000')
+app.get('/', (req, res) => {
+	res.send('Página Home')
+})
+
+app.post('/login', (req, res) => {
+	res.send('Página Login')
+})
+
+app.get('/login', (req, res) => {
+	res.send('Página Login')
+})
+
+app.post('/logado', (req, res) => {
+	res.send('Página Logado')
+})
+
+app.get('/logado', (req, res) => {
+	res.send('Página Logado')
+})
+
+app.listen(3000, () => {
+	console.log('Server running at http://localhost:3000')
 })
