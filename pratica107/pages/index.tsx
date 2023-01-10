7dfb9b1a6b7861df75193b1a1a7c58ffd96f3735
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2023-01-10 04:56:41 -0300

    pratica107

---
+++ pratica107/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Head from 'next/head'
+
+function Home() {
+	return (
+		<>
+			<Head>
+				<title>NextJS App</title>
+			</Head>
+			<main>
+				<h1>NextJS App</h1>
+			</main>
+		</>
+	)
+}
+
+export default Home
