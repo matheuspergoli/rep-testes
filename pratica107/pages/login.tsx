c912370afa3040a74692ce66f746b1fad1b1b4ef
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2023-01-11 06:10:21 -0300

    pratica107

--- pratica107/pages/login.tsx
+++ pratica107/pages/login.tsx
@@ -6,7 +6,7 @@ import jwt from 'jsonwebtoken'
 import Router from 'next/router'
 import { GetServerSideProps } from 'next'
 import { Formik, Form, Field } from 'formik'
-import { registerSchema } from '../validations'
+import { loginSchema } from '../validations'
 import { AuthContext } from '../context/AuthContext'
 import { toFormikValidationSchema } from 'zod-formik-adapter'
 
@@ -35,8 +35,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 	}
 }
 
-function Home() {
-	const { signUp } = React.useContext(AuthContext)
+function Login() {
+	const { signIn } = React.useContext(AuthContext)
 
 	return (
 		<>
@@ -45,37 +45,26 @@ function Home() {
 			</Head>
 			<main className='flex h-screen items-center justify-center'>
 				<section className='w-full max-w-lg'>
-					<h1 className='mb-5 flex items-center justify-center text-xl font-bold'>Cadastre-se na plataforma. &copy;</h1>
+					<h1 className='mb-5 flex items-center justify-center text-xl font-bold'>Faça login na plataforma. &copy;</h1>
 					<Formik
 						initialValues={{
-							name: '',
 							email: '',
 							password: ''
 						}}
-						validationSchema={toFormikValidationSchema(registerSchema)}
+						validationSchema={toFormikValidationSchema(loginSchema)}
 						onSubmit={async (values, { setSubmitting }) => {
 							setSubmitting(true)
-							const response = await signUp(values)
+							const response = await signIn(values)
 							if (response?.error) {
 								alert(response.error)
 								setSubmitting(false)
 								return
 							} else {
-								Router.push('/login')
+								Router.push('/dashboard')
 							}
 						}}>
 						{({ isSubmitting, errors, touched }) => (
 							<Form className='flex flex-col gap-3'>
-								<div className='flex flex-col gap-1'>
-									<Field
-										id='name'
-										name='name'
-										type='text'
-										placeholder='Digite seu nome'
-										className='rounded-md border border-gray-300 p-2'
-									/>
-									{errors.name && touched.name ? <p className='text-red-500'>{errors.name}</p> : null}
-								</div>
 								<div className='flex flex-col gap-1'>
 									<Field
 										id='email'
@@ -96,14 +85,14 @@ function Home() {
 									/>
 									{errors.password && touched.password ? <p className='text-red-500'>{errors.password}</p> : null}
 								</div>
-								<Link href='/login' className='text-right text-sm text-blue-500 underline'>
-									Já possui uma conta? Faça login
+								<Link href='/' className='text-right text-sm text-blue-500 underline'>
+									Não possui uma conta? Cadastre-se
 								</Link>
 								<button
 									type='submit'
 									disabled={isSubmitting}
 									className='rounded-md bg-blue-500 p-2 text-white disabled:bg-opacity-75'>
-									{isSubmitting ? 'Carregando...' : 'Cadastrar'}
+									{isSubmitting ? 'Carregando...' : 'Entrar'}
 								</button>
 							</Form>
 						)}
@@ -114,4 +103,4 @@ function Home() {
 	)
 }
 
-export default Home
+export default Login
